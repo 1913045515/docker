89552211abc238f5d693947301964ecdf1121a50
linzhiqiang <1913045515@qq.com>
2018-01-13 22:23:06 +0800

    update project

--- docker/src/main/java/com/docker/controller/DockerController.java
+++ docker/src/main/java/com/docker/controller/DockerController.java
@@ -17,14 +17,50 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DockerController {
 
     /**
-     * 创建用户
+     * 测试docker打包运行是否成功
      * @param name
      * @return String
      */
-    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiOperation(value="测试docker打包运行是否成功", notes="测试docker打包运行是否成功")
     @RequestMapping(value = "docker",method = RequestMethod.GET)
     @ResponseBody
     public String dockerTest(@ApiParam(value = "用户名称") String name){
         return name+"：创建docker容器并且运行SpringBoot的jar成功。";
+    }
+
+    /**
+     * restful中post风格
+     * @param name
+     * @return String
+     */
+    @ApiOperation(value="restful中post风格", notes="restful中post风格")
+    @RequestMapping(value = "docker",method = RequestMethod.POST)
+    @ResponseBody
+    public String saveTest(@ApiParam(value = "用户名称") String name){
+        return name+"：restful中post风格。";
+    }
+
+    /**
+     * restful中put风格
+     * @param name
+     * @return String
+     */
+    @ApiOperation(value="restful中put风格", notes="restful中put风格")
+    @RequestMapping(value = "name",method = RequestMethod.PUT)
+    @ResponseBody
+    public String updateTest(@ApiParam(value = "用户名称") String name){
+        return name+"：restful中put风格";
+    }
+
+    /**
+     * restful中delete风格
+     * @param name
+     * @return String
+     */
+    @ApiOperation(value="restful中delete风格", notes="restful中delete风格")
+    @RequestMapping(value = "name",method = RequestMethod.DELETE)
+    @ResponseBody
+    public String deleteTest(@ApiParam(value = "用户名称") String name){
+        return name+"：restful中delete风格。";
     }
 }
